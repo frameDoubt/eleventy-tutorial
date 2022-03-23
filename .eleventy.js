fe1981dc39877811b134f255dd4e9077422567b7
frameDoubt <brandon.w.wright4@gmail.com>
2022-03-23 05:53:06 -0700

    add .eleventy.js, .eleventyignore, and .gitignore config change

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,17 @@
 if (process.env.NODE_ENV === 'development') {
   require("dotenv").config();
 }
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.deepDataMerge(true);
+
+  return {
+    dir: {
+      input: "_src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["html", "njk", "11ty.js"]
+  };
+}
